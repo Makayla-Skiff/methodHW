63506454a56095936cd3fbca37dc1e6f9cea5af4
makayla-star <54339097+makayla-star@users.noreply.github.com>
2019-09-27 14:57:07 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace methodsHW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var prog = new Program();
+            prog.myhomework();
+
+            prog.Myhomework2(1400, 12);
+
+
+        }
+
+        public void myhomework()
+        {
+            Console.WriteLine("Homework is cool");
+        }
+        public void Myhomework2(float param1f, float param2f)
+        {
+            var average = param1f / param2f;
+            Console.WriteLine(average);
+        }
+        public void loopmethod(int[] lucky)
+        {
+            foreach (int i in lucky)
+            {
+                if (i == 7)
+                {
+                    Console.WriteLine("i is 7");
+                } else 
+                    {
+                    Console.WriteLine("Lucky You");
+                }
+            }
+
+        }
+
+    }   
+}
